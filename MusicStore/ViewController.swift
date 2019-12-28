ee6d877ab5e6249fff5c35d69a1269882b910b6f
Rajesh M <rajeshm79@outlook.com>
2019-12-28 02:21:44 -0800

    Initial Commit

---
+++ MusicStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicStore
+//
+//  Created by RNSS on 28/12/19.
+//  Copyright © 2019 RNSS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MusicStoreTests/MusicStoreTests.swift
@@ -0,0 +1,34 @@
+//
+//  MusicStoreTests.swift
+//  MusicStoreTests
+//
+//  Created by RNSS on 28/12/19.
+//  Copyright © 2019 RNSS. All rights reserved.
+//
+
+import XCTest
+@testable import MusicStore
+
+class MusicStoreTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
