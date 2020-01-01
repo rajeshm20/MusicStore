a1439650783d40edd018bb506fd117a96700d837
Rajesh M <rajeshm79@outlook.com>
2020-01-01 05:01:00 -0800

    commit track model

---
+++ MusicStore/Models/Track.swift
@@ -0,0 +1,89 @@
+//
+//  Artist.swift
+//  MusicStore
+//
+//  Created by RNSS on 28/12/19.
+//  Copyright © 2019 RNSS. All rights reserved.
+//
+
+import GRDB
+
+// A plain Player struct
+struct Track {
+    // Prefer Int64 for auto-incremented database ids
+    var TrackId: Int
+    var Name: String
+    var AlbumId: Int
+    var MediaTypeId: Int
+    var GenreId: Int
+    var Composer: String
+    var Milliseconds: Int
+    var Bytes: Int
+    var UnitPrice: Int
+    
+
+}
+
+// Hashable conformance supports tableView diffing
+extension Track: Hashable { }
+
+// MARK: - Persistence
+
+// Turn Player into a Codable Record.
+// See https://github.com/groue/GRDB.swift/blob/master/README.md#records
+extension Track: Codable, FetchableRecord, MutablePersistableRecord {
+    // Define database columns from CodingKeys
+    private enum Columns {
+        static let TrackId = Column(CodingKeys.TrackId)
+        static let Name = Column(CodingKeys.Name)
+        static let AlbumId = Column(CodingKeys.TrackId)
+        static let MediaTypeId = Column(CodingKeys.TrackId)
+        static let GenreId = Column(CodingKeys.GenreId)
+        static let Composer = Column(CodingKeys.Composer)
+        static let Milliseconds = Column(CodingKeys.Milliseconds)
+        static let Bytes = Column(CodingKeys.Bytes)
+        static let UnitPrice = Column(CodingKeys.UnitPrice)
+
+    }
+    
+    // Update a player id after it has been inserted in the database.
+    mutating func didInsert(with rowID: Int64, for column: String?) {
+        TrackId = Int(rowID)
+    }
+}
+
+// MARK: - Database access
+
+// Define some useful player requests.
+// See https://github.com/groue/GRDB.swift/blob/master/README.md#requests
+extension Track {
+    static func orderedByName() -> QueryInterfaceRequest<Track> {
+        return Track.order(Columns.Name)
+    }
+    
+    static func orderedByScore() -> QueryInterfaceRequest<Track> {
+        return Track.order(Columns.TrackId.desc, Columns.Name)
+    }
+}
+
+// MARK: - Player Randomization
+
+//extension Player {
+//    private static let names = [
+//        "Arthur", "Anita", "Barbara", "Bernard", "Craig", "Chiara", "David",
+//        "Dean", "Éric", "Elena", "Fatima", "Frederik", "Gilbert", "Georgette",
+//        "Henriette", "Hassan", "Ignacio", "Irene", "Julie", "Jack", "Karl",
+//        "Kristel", "Louis", "Liz", "Masashi", "Mary", "Noam", "Nicole",
+//        "Ophelie", "Oleg", "Pascal", "Patricia", "Quentin", "Quinn", "Raoul",
+//        "Rachel", "Stephan", "Susie", "Tristan", "Tatiana", "Ursule", "Urbain",
+//        "Victor", "Violette", "Wilfried", "Wilhelmina", "Yvon", "Yann",
+//        "Zazie", "Zoé"]
+//
+//    static func randomName() -> String {
+//        return names.randomElement()!
+//    }
+//
+//    static func randomScore() -> Int {
+//        return 10 * Int.random(in: 0...100)
+//    }
+//}
